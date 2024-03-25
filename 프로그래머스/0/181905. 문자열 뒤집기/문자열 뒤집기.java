56e5127fe7a45c56410e1a3ac0771736c95bfa1d
이지수 Lee Jisu <ljsgoo0617@gmail.com>
2024-03-25 22:41:44 +0900

    [level 0] Title: 문자열 뒤집기, Time: 12.20 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,8 +1,7 @@
 class Solution {
     public String solution(String my_string, int s, int e) {
         String answer = "";
-        String str = my_string.substring(s, (e+1));
-        StringBuffer sb = new StringBuffer(str);
+        StringBuffer sb = new StringBuffer(my_string.substring(s, (e+1)));
         String strRev = sb.reverse().toString();
         
         answer = my_string.substring(0,s) + strRev + my_string.substring(e+1);
